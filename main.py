5b673d3834e6d9bbd09530f2e5d265ccd3a862a5
Adam Calcasola <adam.calcasola@gmail.com>
2023-10-10 22:54:58 -0400

    major refactor

--- main.py
+++ main.py
@@ -1,45 +1,54 @@
 from art import logo
 import random
 
+def difficulty():
+    difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+    if difficulty == 'easy':
+        return 10
+    elif difficulty == 'hard':
+        return 5
+    else:
+        print("Invalid command")
+        return 0
+    
+def check_answer(attempts, number, guess):
+    if guess > number:
+        print("Too high.")
+        return attempts - 1
+    elif guess < number:
+        print("Too low.")
+        return attempts - 1
+    else:
+        print(f"You guessed the secret number, {number}!")
+        print("You Win!")
+
 def guess_the_number():
     print(logo)
     print("Welcome to the Number Guessing Game!")
     print("I'm thinking of a number between 1 and 100.")
-    difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
-    if difficulty == 'easy':
-        attempts = 10
-    elif difficulty == 'hard':
-        attempts = 5
-    else:
-        print("Invalid command")
-        attempts = 0
-
+    attempts = difficulty()
     number = random.randint(1, 100)
+    guess = 0
 
-    while attempts > 0:
+    while guess != number and attempts > 0:
         if attempts > 1:
             print(f"You have {attempts} attempts remaining to guess the number.")
         else:
             print(f"You have {attempts} more attempt to guess the number!")
+        
         guess = int(input("Make a guess: "))
-        attempts -= 1
-        if guess == number:
-            print("You Win!")
-            break
-        elif guess > number:
-            print("Too high.")
-        elif guess < number:
-            print("Too low.")
+        
+        attempts = check_answer(attempts, number, guess)
 
-        if attempts > 0:
-            print("Guess again.")
-        else:
+        if attempts == 0:
             print("You've run out of guesses, you lose.")
+            play_again = input("Want to play again? Type 'y' or 'n': ")
+            if play_again == "y":
+                guess_the_number()
+                return
+            else:
+                return
+        elif guess != number:
+            print("Guess again.")
 
-    play_again = input("Want to play again? Type 'y' or 'n': ")
-    if play_again == "y":
-        guess_the_number()
-    else:
-        return
-    
 guess_the_number()
